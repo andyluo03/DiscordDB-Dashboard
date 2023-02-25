0412495084aa5a14cd0a15d4849e7a087b93fe79
imedra <marcbernardino2005@gmail.com>
2023-02-25 12:30:01 -0600

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { useEffect } from "react";
+
+export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    window.history.scrollRestoration = 'manual'
+  }, []);
+  return <Component {...pageProps} />
+}
